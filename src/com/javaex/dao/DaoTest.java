52db48e5202150e66d231ff561fbee8eba384a32
Maroban <dbtmdqja207@naver.com>
2021-07-19 20:48:59 +0900

    07/19 수정

--- src/com/javaex/dao/DaoTest.java
+++ src/com/javaex/dao/DaoTest.java
@@ -1,34 +1,16 @@
 package com.javaex.dao;
 
-import com.javaex.vo.UserVo;
+import java.util.List;
+
+import com.javaex.vo.BoardVo;
 
 public class DaoTest {
 
 	public static void main(String[] args) {
 
+		BoardDao boardDao = new BoardDao();
 
-		UserDao userDao = new UserDao();
-		/*
-		UserVo userVo = userDao.getUser("aaaaa5", "1234");
-
-		System.out.println(userVo);
-
-		UserVo userVo2 = userDao.getUser(1);
-		System.out.println(userVo2);
-		*/
-
-		int no = 1;
-		String name = "강동원";
-		String password = "1";
-		String gender = "female";
-
-		UserVo userVo3 = new UserVo(no, name, password, gender);
-
-		userDao.update(userVo3);
-
-		UserVo userVo4 = userDao.getUser(1);
-
-		System.out.println(userVo4);
+		System.out.println(boardDao.search("안").get(1).getNo());
 
 	}
 
