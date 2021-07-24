54e4a21362215e285f43face286780778ecd968d
Maroban <dbtmdqja207@naver.com>
2021-07-25 02:11:34 +0900

    오류 수정

--- src/com/javaex/dao/DaoTest.java
+++ src/com/javaex/dao/DaoTest.java
@@ -1,16 +1,12 @@
 package com.javaex.dao;
 
-import java.util.List;
-
-import com.javaex.vo.BoardVo;
-
 public class DaoTest {
 
 	public static void main(String[] args) {
 
 		BoardDao boardDao = new BoardDao();
 
-		System.out.println(boardDao.search("안").get(1).getNo());
+		System.out.println(boardDao.boardList("안").get(1).getNo());
 
 	}
 
